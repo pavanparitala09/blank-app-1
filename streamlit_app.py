0319877dd5a60ef62a0c1fd825f6e46e527b6ecd
PARITALA PAVAN KUMAR <pavankumarparitala2580@gmail.com>
2026-04-16 10:46:29 +0530

    Change title from 'Internal' to 'External'

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Cloud Computing Lab Internal")
+st.title("🎈 Cloud Computing Lab External")
 st.write(
     " This Is An Sample web Application.Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
